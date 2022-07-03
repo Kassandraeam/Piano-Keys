7debe4b6aa71117f41bf0b76e7b4db972bb0eda1
Kas Eam <KassandraEam@gmail.com>
2022-07-02 20:36:58 -0500

    Completed 3/21 tasks.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+
 const notes = [];
+
 keys.forEach(function(key){
   notes.push(document.getElementById(key));
 })
@@ -10,9 +12,19 @@ keys.forEach(function(key){
 
 // Write a named function with event handler properties
 
+const keyPlay = function(event){
+  event.target.style.backgroundColor = 'green';
+}
+
+const keyReturn = function(event){
+  event.target.style.backgroundColor = '';
+}
 
 // Write a loop that runs the array elements through the function
 
+const step3 = (note) => {
+
+}
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
